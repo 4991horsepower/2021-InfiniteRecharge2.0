49792d4f3d170cceb32d6206f26d7cf6efae0972
ddipofi2023 <76458279+ddipofi2023@users.noreply.github.com>
2020-12-28 18:50:06 -0500

    Changes Victors to Talons
    Takes PWMVictorSPX designation and reassigns it to Talons

--- Test1/src/main/java/frc/robot/Robot.java
+++ Test1/src/main/java/frc/robot/Robot.java
@@ -8,7 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -21,19 +21,19 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class Robot extends TimedRobot {
   private Joystick m_leftStick;
   private Joystick m_rightStick;
-  private  SpeedController m_frontLeft = new PWMVictorSPX(1);
-  private SpeedController m_rearLeft = new PWMVictorSPX(2);
+  private  SpeedController m_frontLeft = new Talon(1);
+  private SpeedController m_rearLeft = new Talon(2);
   private  SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
   
-  private  SpeedController m_frontRight = new PWMVictorSPX(3);
-  private  SpeedController m_rearRight = new PWMVictorSPX(4);
+  private  SpeedController m_frontRight = new Talon(3);
+  private  SpeedController m_rearRight = new Talon(4);
   private  SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
    
   private   DifferentialDrive m_myRobot = new DifferentialDrive(m_left, m_right);
 
   @Override
   public void robotInit() {
-   // m_myRobot = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+   // m_myRobot = new DifferentialDrive(new Talon(0), new Talon(1));
     m_leftStick = new Joystick(0);
     m_rightStick = new Joystick(1);
       
