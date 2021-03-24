d482465d0760679ccb32abe7802c5f5ba212aa33
sle2023 <76458311+sle2023@users.noreply.github.com>
2021-03-24 19:28:59 -0400

    Clean up code

--- Test1/src/main/java/frc/robot/Robot.java
+++ Test1/src/main/java/frc/robot/Robot.java
@@ -1,6 +1,5 @@
 
 package frc.robot;
-//package edu.wpi.first.wpilibj.examples.hatchbottraditional.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
@@ -27,14 +26,9 @@ import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-//import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
-//import java.io.BufferedReader;
-//import java.io.FileInputStream;
-//import java.io.InputStreamReader;
 import java.util.Locale;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -89,43 +83,18 @@ public class Robot extends TimedRobot {
   private Solenoid intake_out = new Solenoid(3);
 
   private double heading;
-  private double prev_enc_right;
-  private double prev_enc_left;
-
-  private double pos_x;
-  private double pos_y;
 
   private PrintStream autoWriter;
   private Scanner autoScan;
-  private boolean prevYPos = false; 
-
-  //slalom path 
-  //private double x_targets[] = {2.0, 5.0, 5.0, 0.0, -5.0, -5.0, -2.0, 0.0};
-  //private double y_targets[] = {3.0, 6.0, 10.0, 15.0, 10.0, 6.0, 3.0, 0.0};
-
-  //barrel racing path
-  //private double x_targets[] = {0.0,2.5,5.0,2.5,0.0,0.0,-2.5,-5.0,-2.5,0.0,5.0,2.5,0.0,5.0,2.5,0.0,0.0};
-  //private double y_targets[] = {9.0,11.5,9.0,7.5,9.0,17.0,19.5,17.0,14.5,17.0,22.0,24.5,22.0,0.0};
-
-  //bounce path
-  //private double x_targets[] = {0, -5, -5, 0};
-  //private double y_targets[] = {10, 0, 10, 0};
-  
-  private double target_x = 0;
-  private double target_y = 0;
+  private boolean prevYPos = false;
  
   // Kauail Labs AHRS (for heading and rate)
   private AHRS ahrs = new AHRS(SPI.Port.kMXP);
 
-  private double shooterSpeed = -4700;
+  private double targetShooterSpeed = -4700;
   private double shooterError = 100;
 
   private double hoodAngle = 85;
-
-  private boolean isRecording = false;
-  //private String autoPath = "/home/lvuser/bouncePath.txt"; //"/home/lvuser/barrelRacingPath.txt";
-  private double autoTime;
-
   private double maxSpeed = 1000;
 
   @Override
@@ -191,34 +160,25 @@ public class Robot extends TimedRobot {
 		m_frontLeft.config_kP(0, 2.0);
 		m_frontLeft.config_kI(0, 0.0);
     m_frontLeft.config_kD(0, 10);
-    m_frontLeft.configClosedloopRamp(0.2);
+    m_frontLeft.configClosedloopRamp(0.1);
 
     
     m_rearRight.config_kF(0, 1023.0/maxSpeed);
 		m_rearRight.config_kP(0, 2.0);
 		m_rearRight.config_kI(0, 0.0);
     m_rearRight.config_kD(0, 10);
-    m_rearRight.configClosedloopRamp(0.2);
+    m_rearRight.configClosedloopRamp(0.1);
     
     String [] autoModes = {"bouncePath", "barrelRacingPath", "slalomPath", "scratchpad"};
     SmartDashboard.putStringArray("Auto List", autoModes);
   }
   
-
   @Override
   public void autonomousInit() {
     ahrs.zeroYaw(); 
-    prev_enc_left = m_frontLeft.getSelectedSensorPosition();
-    prev_enc_right = m_rearRight.getSelectedSensorPosition();
-    pos_x = 0;
-    pos_y = 0;
-    autoTime = System.currentTimeMillis() + 500;
 
-    try{
-        System.out.println("isRecording = " + isRecording);
-        
+    try{        
         String filePath = "/home/lvuser/" + SmartDashboard.getString("Auto Selector", "scratchpad") + ".txt";
-
         autoScan = new Scanner(new File(filePath));
         autoScan.useLocale(Locale.US);
     }
@@ -232,93 +192,41 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {
     heading = ahrs.getYaw();
 
-    double enc_right = m_rearRight.getSelectedSensorPosition();
-    double enc_left = m_frontLeft.getSelectedSensorPosition();
-
     double left_right;
     double front_back;
-
-    double diff_enc_left = enc_left - prev_enc_left;
-    double diff_enc_right = enc_right - prev_enc_right;
-
-    double diff_enc = ((diff_enc_right - diff_enc_left)/2.0) / 900.0;
-
-    pos_y += diff_enc * Math.cos(heading * Math.PI/180.0);
-    pos_x += diff_enc * Math.sin(heading * Math.PI/180.0);
-
-    prev_enc_left = enc_left;
-    prev_enc_right = enc_right;
-
-    double fwd = 0.3;
-
-    double x_diff;
-    double y_diff;
     double targetHeading;
 
-      x_diff = target_x - pos_x;
-      y_diff = target_y - pos_y;
+    if(autoScan.hasNextDouble())
+    {
+      left_right = autoScan.nextDouble();
+      front_back = autoScan.nextDouble();
 
-      targetHeading = heading - (Math.atan2(x_diff, y_diff)*180.0/Math.PI);
-  
-      fwd = 0.3 * (1.0 - (Math.abs(targetHeading) / 360.0));
-
-      if(targetHeading > 180)
-        targetHeading -= 360;
-      if(targetHeading < -180)
-        targetHeading += 360;
-  
-      /*if(Math.abs(targetHeading) > 90)
+      targetHeading = autoScan.nextDouble();
+      double headingError = targetHeading - heading;
+      if(headingError > 180)
       {
-        targetHeading -= 180;
-        fwd = -0.3;
-        if(targetHeading < -180)
-        {
-          targetHeading += 360;
-        }
-      }*/
-      
-      double targetDistance = Math.sqrt(x_diff * x_diff + y_diff * y_diff);
-      double targetMinDistance = 0.7;
-
-        if(autoScan.hasNextDouble())
-        {
-          left_right = autoScan.nextDouble();
-          front_back = autoScan.nextDouble();
-
-          targetHeading = autoScan.nextDouble();
-          double headingError = targetHeading - heading;
-          if(headingError > 180)
-          {
-            headingError -= 360;
-          }
-          if(headingError < -180)
-          {
-            headingError += 360;
-          }
-        
-          left_right += 0.01*headingError;
-
-        }
-        else
-        {
-          left_right = 0;
-          front_back = 0;
-        }
-
-  
-        double left = -front_back - left_right;
-        double right = front_back - left_right;
+        headingError -= 360;
+      }
+      if(headingError < -180)
+      {
+        headingError += 360;
+      }
     
-        /*
-        if(Math.abs(left) < 0.1)
-          left = 0;
-        if(Math.abs(right) < 0.1)
-          right = 0;
-        */
+      left_right += 0.01*headingError;
 
-        m_rearRight.set(ControlMode.Velocity, right*maxSpeed);
-        m_frontLeft.set(ControlMode.Velocity, left*maxSpeed);
     }
+    else
+    {
+      left_right = 0;
+      front_back = 0;
+    }
+
+    double left = -front_back - left_right;
+    double right = front_back - left_right;
+
+    m_rearRight.set(ControlMode.Velocity, right*maxSpeed);
+    m_frontLeft.set(ControlMode.Velocity, left*maxSpeed);
+  }
 
   @Override
   public void teleopPeriodic() {
@@ -328,20 +236,11 @@ public class Robot extends TimedRobot {
     double front_back = reverse < 0.01 ? forward : -reverse;
     double left_right = m_driverController.getX(Hand.kLeft);
     
-
     front_back = front_back > 0 ? Math.pow(Math.abs(front_back), 3) : -Math.pow(Math.abs(front_back), 3);
     left_right = left_right > 0 ? 0.5*Math.pow(Math.abs(left_right), 3) : -0.5*Math.pow(Math.abs(left_right), 3);
-    
 
     double left = -front_back - left_right;
     double right = front_back - left_right;
-
-    /*
-    if(Math.abs(left) < 0.1)
-      left = 0;
-    if(Math.abs(right) < 0.1)
-      right = 0;
-    */
 
     m_rearRight.set(ControlMode.Velocity, maxSpeed*right);
     m_frontLeft.set(ControlMode.Velocity, maxSpeed*left);
@@ -355,7 +254,7 @@ public class Robot extends TimedRobot {
     
     if(wheelState == true)
     {
-      m_pidController.setReference(shooterSpeed, ControlType.kVelocity);
+      m_pidController.setReference(targetShooterSpeed, ControlType.kVelocity);
     }
     else
     {
@@ -387,7 +286,10 @@ public class Robot extends TimedRobot {
     {
       m_spinner.set(0);
     }
-    if (Math.abs(m_encoder.getVelocity()-shooterSpeed) < shooterError)
+
+    double shooterSpeed = m_encoder.getVelocity();
+
+    if (Math.abs(shooterSpeed-targetShooterSpeed) < shooterError)
     {
       m_wheel.set(-1);
 
@@ -455,12 +357,9 @@ public class Robot extends TimedRobot {
 
       if (m_copilotController.getYButton() && prevYPos == false){
         ahrs.zeroYaw();
-        try{
-        System.out.println("isRecording = " + isRecording);
-        
-        String filePath = "/home/lvuser/" + SmartDashboard.getString("Auto Selector", "scratchpad") + ".txt";
-
-        autoWriter = new PrintStream(filePath);
+        try{        
+          String filePath = "/home/lvuser/" + SmartDashboard.getString("Auto Selector", "scratchpad") + ".txt";
+          autoWriter = new PrintStream(filePath);
         }
         catch(FileNotFoundException e)
         {
